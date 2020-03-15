43a8992f3a583d989190bbf356d25eec36294cc0
NicholeSteig <nichole.steig@gmail.com>
2020-03-14 20:42:49 -0700

    framework works

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
+import "./pages/pages.css"
+// import Contact from "./pages/Contact";
+// import Portfolio from "./pages/Portfolio";
 
 
 function App() {
     return (
         <Router>
-            <div>
-                <Navbar />
+            <div className="container">
+                {/* <Navbar /> */}
                 <Wrapper>
                     <Route exact path="/" component={Home} />
-                    <Route exact path="/portfolio" component={Portfolio} />
-                    <Route exact path="/contact" component={Contact} />
+                    <Route exact path="/home" component={Home} />
+                    {/* <Route exact path="/portfolio" component={Portfolio} />
+                    <Route exact path="/contact" component={Contact} /> */}
                 </Wrapper>
             </div>
         </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-import API from "../Util/API";
-import Navbar from "../Components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from "react-bootstrap/Card";
+import Navbar from "../components/Navbar";
+import "../pages/pages.css"
 
 const Home = () => {
     return (
         <div>
-            {/* <body background="../assets/background-blur-clean-clear-531880.jpg"></body> */}
-            <Card style={{ width: '30rem' }}>
+            <Navbar></Navbar>
+            <Card style={{ width: '60rem' }}>
                 <Card.Title>About Me</Card.Title>
                 <Card.Text>
                     Blah blah blah
